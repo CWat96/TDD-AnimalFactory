79e7d5151fc96e7781d660d0036170e6a3400d6b
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-11-30 13:28:42 -0500

    Checkpointing my work

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -11,33 +11,85 @@ import java.util.Date;
  */
 public class CatTest {
     // TODO - Create tests for `void setName(String name)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
-
-
     @Test
-    public void constructorTest() {
-        // Given (cat data)
-        String givenName = "Zula";
-        Date givenBirthDate = new Date();
-        Integer givenId = 0;
-
-        // When (a cat is constructed)
-        Cat cat = new Cat(givenName, givenBirthDate, givenId);
-
-        // When (we retrieve data from the cat)
-        String retrievedName = cat.getName();
-        Date retrievedBirthDate = cat.getBirthDate();
-        Integer retrievedId = cat.getId();
-
-        // Then (we expect the given data, to match the retrieved data)
-        Assert.assertEquals(givenName, retrievedName);
-        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
-        Assert.assertEquals(givenId, retrievedId);
+    public void setNameTest() {
+        //Given
+        String name = "Zula";
+        Date birthDate = new Date();
+        Integer id = 2525;
+        Cat Zula = new Cat(name, birthDate, id);
+        //When
+        String Actual = "Zula";
+        String Expected = Zula.getName();
+        //Then
+        Assert.assertEquals(Actual, Expected);
     }
 
-}
+    // TODO - Create tests for `speak`
+    @Test
+    public void testSpeak() {
+        //Given
+        String name = "Zula";
+        Date birthDate = new Date();
+        Integer id = 2525;
+        Cat zula = new Cat(name, birthDate, id);
+        String expected = "meow!";
+
+        //When
+        String Actual = zula.speak();
+        //Then
+        Assert.assertEquals(expected, Actual);
+    }
+
+        // TODO - Create tests for `setBirthDate(Date birthDate)`
+    @Test
+    public void testSetBirthDate() {
+            //Given
+            String name = "Zula";
+            Date birthDate = new Date();
+            Integer id = 2525;
+            Cat cat = new Cat(name, birthDate, id);
+            Date expectedBirthDate = new Date();
+            //When
+            cat.setBirthDate(expectedBirthDate);
+            //Then
+            Date actualBirthdate = cat.getBirthDate();
+            Assert.assertEquals(expectedBirthDate, actualBirthdate);
+        }
+
+        // TODO - Create tests for `void eat(Food food)`
+    @Test
+    public void testEat() {
+        //Given
+        String name = "Zula";
+        Date birthDate = new Date();
+        Integer id = 2525;
+    }
+
+
+        // TODO - Create tests for `Integer getId()`
+        // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+        // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    }
+
+//    @Test
+//    public void constructorTest() {
+//        // Given (cat data)
+//        String givenName = "Zula";
+//        Date givenBirthDate = new Date();
+//        Integer givenId = 0;
+//
+//        // When (a cat is constructed)
+//        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+//
+//        // When (we retrieve data from the cat)
+//        String retrievedName = cat.getName();
+//        Date retrievedBirthDate = cat.getBirthDate();
+//        Integer retrievedId = cat.getId();
+//
+//        // Then (we expect the given data, to match the retrieved data)
+//        Assert.assertEquals(givenName, retrievedName);
+//        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
+//        Assert.assertEquals(givenId, retrievedId);
+//    }
+//    }
