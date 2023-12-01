6aa90700e7d9fedef8d39023ca755a7c8c04d03a
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-01 11:02:54 -0500

    Checkpointing my work

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,29 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void testCreateDog() {
+        //Given
+        String name = ""; // create dog name
+        Date birthDate = new Date(); // giving the dog a birthdate
+        Dog dog = AnimalFactory.createDog(name, birthDate); // Dog is in animal factory with attributes
+        //When
+        String expectedName = dog.getName();
+        //Then
+        Assert.assertNotNull(dog);
+        Assert.assertTrue(true);
+
+    }
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
 }
